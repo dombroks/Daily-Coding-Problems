3a7fcabd7bdcaa692da82a9781b9df0daadf31b1
dombroks <tinoutech@gmail.com>
2020-09-09 13:37:52 +0100

    Updating

---
+++ Google_Problems/Google_Problem_03.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+
+This problem was asked by Google.
+
+A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+
+Given the root to a binary tree, count the number of unival subtrees.
+
+For example, the following tree has 5 unival subtrees:
+
+   0
+  / \
+ 1   0
+    / \
+   1   0
+  / \
+ 1   1
+"""
+
+# node structure.
+class Node:
+    def __init__(self, val,left=None,right=None):
+        self.val = val
+        self.left = left
+        self.right= right
+
+def is_unival(root):
+    return unival_helper(root, root.val)
+
+def unival_helper(root, value):
+    if root is None:
+        return True
+    if root.val == value:
+        return unival_helper(root.left, value) and unival_helper(root.right, value)
+    return False
+
+def count_unival_subtrees(root):
+    if root is None:
+        return 0
+    left = count_unival_subtrees(root.left)
+    right = count_unival_subtrees(root.right)
+    return 1 + left + right if is_unival(root) else left + right
+ 
+
+if __name__ == "__main__":
+  node = Node(0, Node(1), Node(0,Node(1,Node(1),Node(1)),Node(0)))
+  print( count_unival_subtrees(node))
