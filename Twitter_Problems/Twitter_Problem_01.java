9fb02944aab6e31688b213204aff64aba6c09d3a
dombroks <tinoutech@gmail.com>
2020-09-16 22:54:15 +0100

    organizing

--- Twitter_Problems/Twitter_Problem_01.java
+++ Twitter_Problems/Twitter_Problem_01.java
@@ -14,12 +14,12 @@ import java.util.Random;
 
 public class Twitter_Problem_01 {
 
-    static class CustomizedBuffer {
+    static class OrderLog {
         int[] ids;
         int numberOfRecords;
         int endPointer;
 
-        public CustomizedBuffer(int numberOfRecords) {
+        public OrderLog(int numberOfRecords) {
             this.numberOfRecords = numberOfRecords;
             this.ids = new int[numberOfRecords];
             this.endPointer = 0;
@@ -42,14 +42,14 @@ public class Twitter_Problem_01 {
 
     public static void main(String[] args) {
         Random random = new Random();
-        CustomizedBuffer buffer = new CustomizedBuffer(12);
-        for (int i = 0; i < buffer.numberOfRecords; i++) {
-            //fill the buffer with random numbers between 0 and 100
-            buffer.record(random.nextInt(100));
-            //print buffer elements
-            System.out.println(buffer.ids[i]);
+        OrderLog log = new OrderLog(12);
+        for (int i = 0; i < log.numberOfRecords; i++) {
+            //fill the log with random numbers between 0 and 100
+            log.record(random.nextInt(100));
+            //print log elements
+            System.out.println(log.ids[i]);
         }
         int number = 2;
-        System.out.println("the " + number + "th last element is: " + buffer.get_last(number));
+        System.out.println("the " + number + "th last element is: " + log.get_last(number));
     }
 }
