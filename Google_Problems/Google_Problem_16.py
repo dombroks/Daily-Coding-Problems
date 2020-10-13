66feeeab05de8cd198f1a0ce8583d21806029941
dombroks <tinoutech@gmail.com>
2020-10-13 19:15:35 +0100

    solving new problem from Google

--- Google_Problems/Google_Problem_16.py
+++ Google_Problems/Google_Problem_16.py
@@ -10,7 +10,7 @@ You may assume each element in the array is distinct.
 
 For example, a sorted list has zero inversions. The array [2, 4, 1, 3, 5] has three inversions: (2, 1), (4, 1), and (4, 3). The array [5, 4, 3, 2, 1] has ten inversions: every distinct pair forms an inversion.
 
-Note : Faster than O(N^2) means O(n log n) or faster if you could xD
+Note added by me : Faster than O(N^2) means O(n log n) or faster if you could xD
 """
 
 def merge_sort(arr):
