01babe7e32dc0e8d6252fb2b751cd9987a5be2cf
dombroks <tinoutech@gmail.com>
2020-10-14 17:26:37 +0100

    Solving the first problem from Two Sigma

---
+++ Two_Sigma_Problems/Two_Sigma_00.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+This problem was asked by Two Sigma.
+
+Using a function rand5() that returns an integer from 1 to 5 (inclusive) with uniform probability, implement a function rand7() that returns an integer from 1 to 7 (inclusive).
+"""
+
+import random
+
+def rand5():
+  return random.randint(1,5)
+
+def rand7():
+  return random.randint(rand5(),7)
+
+
+# Driver code
+print(rand7())
