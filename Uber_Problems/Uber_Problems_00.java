b43ab84579106cb01f370a2ea355cc1f4fd93e5b
dombroks <tinoutech@gmail.com>
2020-12-01 19:27:34 +0100

    cleaning

--- Uber_Problems/Uber_Problems_00.java
+++ Uber_Problems/Uber_Problems_00.java
@@ -6,8 +6,6 @@ Given an array of integers, return a new array such that each element at index i
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 */
 
-
-
 public class Problem_00 {
     public static int[] productArray(int[] array) {
         int [] array1 = new int[array.length] ;
